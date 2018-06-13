bf0e74874dc327254a45ed25b043c4d409f46a58
MrLurch81 <lurch81@gmail.com>
2018-06-13 21:20:44 +0200

    changed directories

--- run_proxy_docker.sh
+++ run_proxy_docker.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 scriptdir=/growatt-proxy
-logfile=$scriptdir/logs/`date "+%Y%m%d.log"`
+configdir=/config
+logdir=$configdir/logs
+datadir=$configdir/datafiles
+if [! -d $logdir]
+then
+  mkdir $logdir
+fi
+if [! -d $configdir]
+then
+  mkdir $configdir
+fi
+logfile=$logdir/`date "+%Y%m%d.log"`
 export PERL5LIB=$scriptdir/scripts
 # Remove --remote for standalone server.
-perl $scriptdir/scripts/growatt_server.pl --debug --remote --datadir=/growatt-proxy/datafiles 2>&1 | tee -a $logfile
+perl $scriptdir/scripts/growatt_server.pl --debug --remote --datadir=$datadir 2>&1 | tee -a $logfile
